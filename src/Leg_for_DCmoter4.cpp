942327288fd369c3a7a4ec362b950b8e7b20a871
yukichi6105 <107849799+YUKICHI6105@users.noreply.github.com>
2023-06-18 18:41:11 +0900

    paramの追加とfonfigファイルの追加

--- src/Leg_for_DCmoter4.cpp
+++ src/Leg_for_DCmoter4.cpp
@@ -1,6 +1,9 @@
 #include <memory>
 #include <string>
 
+#include <chrono>
+#include <functional>
+
 #include "rclcpp/rclcpp.hpp"
 #include "can_plugins2/msg/frame.hpp"
 #include <sensor_msgs/msg/joy.hpp>
@@ -8,19 +11,13 @@
 
 
 using std::placeholders::_1;
+using namespace std::chrono_literals;
 
 /* This example creates a subclass of Node and uses std::bind() to register a
 * member function as a callback from the timer. */
 
 class pubsub : public rclcpp::Node
 {
-  public:
-    pubsub() : Node("l4dc4_node"), count_(0)
-    {
-      publisher_ = this->create_publisher<can_plugins2::msg::Frame>("can_tx", 10);
-      subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&pubsub::joy_callback, this, _1));
-    }
-
   private:
     void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
     rclcpp::TimerBase::SharedPtr timer_;
@@ -28,18 +25,50 @@ class pubsub : public rclcpp::Node
     rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriber_;
     size_t count_;
     int count = 0;
+    float maxSpeed = 0.0f;//厳密にはちょっと違う。
+    uint32_t upperRightID;
+    uint32_t upperLeftID;
+    uint32_t lowerLeftID;
+    uint32_t lowerRightID;
+  public:
+    pubsub() : Node("l4dc4_node"), count_(0)
+    {
+      publisher_ = this->create_publisher<can_plugins2::msg::Frame>("can_tx", 10);
+      subscriber_ = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&pubsub::joy_callback, this, _1));
+      this->declare_parameter("maxSpeed", 6.28f);
+      this->declare_parameter("upperRight", 0x160);
+      this->declare_parameter("upperLeft", 0x164);
+      this->declare_parameter("lowerLeft", 0x168);
+      this->declare_parameter("lowerRight", 0x16c);
+      timer_ = this->create_wall_timer(1000ms, std::bind(&pubsub::timer_callback, this));
+    }
+
+    void timer_callback()
+    {
+      maxSpeed = this->get_parameter("maxSpeed").as_double();
+      upperRightID = this->get_parameter("upperRight").as_int();
+      upperLeftID = this->get_parameter("upperLeft").as_int();
+      lowerLeftID = this->get_parameter("lowerLeft").as_int();
+      lowerRightID = this->get_parameter("lowerRight").as_int();
+      RCLCPP_INFO(this->get_logger(), "max speed %f!", maxSpeed);
+      RCLCPP_INFO(this->get_logger(), "upperRight %d!", upperRightID);
+      RCLCPP_INFO(this->get_logger(), "upperLeft %d!", upperLeftID);
+      RCLCPP_INFO(this->get_logger(), "lowerLeft %d!", lowerLeftID);
+      RCLCPP_INFO(this->get_logger(), "lowerRight %d!", lowerRightID);
+      //std::vector<rclcpp::Parameter> all_new_parameters{rclcpp::Parameter("my_parameter", "world")};
+      //this->set_parameters(all_new_parameters);
+    }
 };
 
 void pubsub::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
   {  
 //    RCLCPP_INFO(this->get_logger(), "I heard:");
-
     if(msg->buttons[2]==1)
     {
-      publisher_->publish(get_frame(0x160,static_cast<uint8_t>(5)));
-      publisher_->publish(get_frame(0x164,static_cast<uint8_t>(5)));
-      publisher_->publish(get_frame(0x168,static_cast<uint8_t>(5)));
-      publisher_->publish(get_frame(0x16c,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(upperRightID,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(upperLeftID,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(lowerLeftID,static_cast<uint8_t>(5)));
+      publisher_->publish(get_frame(lowerRightID,static_cast<uint8_t>(5)));
       publisher_->publish(get_frame(0x180,static_cast<uint8_t>(5)));
       publisher_->publish(get_frame(0x200,static_cast<uint8_t>(5)));
       publisher_->publish(get_frame(0x210,static_cast<uint8_t>(5)));
@@ -47,10 +76,10 @@ void pubsub::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
 
     if(msg->buttons[1]==1)
     {
-      publisher_->publish(get_frame(0x160,static_cast<uint8_t>(0)));
-      publisher_->publish(get_frame(0x164,static_cast<uint8_t>(0)));
-      publisher_->publish(get_frame(0x168,static_cast<uint8_t>(0)));
-      publisher_->publish(get_frame(0x16c,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(upperRightID,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(upperLeftID,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(lowerLeftID,static_cast<uint8_t>(0)));
+      publisher_->publish(get_frame(lowerRightID,static_cast<uint8_t>(0)));
       publisher_->publish(get_frame(0x140,static_cast<uint8_t>(0)));
       publisher_->publish(get_frame(0x200,static_cast<uint8_t>(0)));
       publisher_->publish(get_frame(0x210,static_cast<uint8_t>(0)));
@@ -74,10 +103,10 @@ void pubsub::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
     }
     if((x != 0) || (y != 0)){
       //右回転
-      publisher_->publish(shirasu_frame(0x161, 6.28f*(y-x+r)));
-      publisher_->publish(shirasu_frame(0x165, 6.28f*(-x-y+r)));
-      publisher_->publish(shirasu_frame(0x169, 6.28f*(x-y+r)));
-      publisher_->publish(shirasu_frame(0x16d, 6.28f*(x+y+r)));
+      publisher_->publish(shirasu_frame(upperRightID+1, maxSpeed*(y-x+r)));
+      publisher_->publish(shirasu_frame(upperLeftID+1, maxSpeed*(-x-y+r)));
+      publisher_->publish(shirasu_frame(lowerLeftID+1, maxSpeed*(x-y+r)));
+      publisher_->publish(shirasu_frame(lowerRightID+1, maxSpeed*(x+y+r)));
       //chatter.publish(get_frame(0x101, x/static_cast<float>(sqrt(2))-y/static_cast<float>(sqrt(2))));
       //100右上、110左上、120左下、130右下
       count = 0;
@@ -89,10 +118,10 @@ void pubsub::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg)
     }
     else{
       if(count == 0){
-        publisher_->publish(shirasu_frame(0x161, 0.0f));
-        publisher_->publish(shirasu_frame(0x165, 0.0f));
-        publisher_->publish(shirasu_frame(0x169, 0.0f));
-        publisher_->publish(shirasu_frame(0x16d, 0.0f));
+        publisher_->publish(shirasu_frame(upperRightID+1, 0.0f));
+        publisher_->publish(shirasu_frame(upperLeftID+1, 0.0f));
+        publisher_->publish(shirasu_frame(lowerLeftID+1, 0.0f));
+        publisher_->publish(shirasu_frame(lowerRightID+1, 0.0f));
         count = 1;
       }
     }
