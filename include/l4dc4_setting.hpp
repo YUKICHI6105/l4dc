44505dc743e1493c0bb053e941149c6a1193f907
yukichi6105 <107849799+YUKICHI6105@users.noreply.github.com>
2023-06-21 17:44:09 +0900

    valve_frameの追加

--- include/l4dc4_setting.hpp
+++ include/l4dc4_setting.hpp
@@ -1,4 +1,7 @@
 #include <rclcpp/rclcpp.hpp>
+#include <array>
+#include <can_plugins2/msg/frame.hpp>
+#define CAN_MTU 8
 
 enum ShirasuMode{
     dis,
@@ -10,9 +13,24 @@ enum ShirasuMode{
     pos,
 };
 
-struct ShirasuLegID{
-    uint32_t upperRightID;
-    uint32_t upperLeftID;
-    uint32_t lowerLeftID;
-    uint32_t lowerRightID;
+struct ShirasuLegID{//シラスのID
+    uint32_t upperRightID;//右上
+    uint32_t upperLeftID;//左上
+    uint32_t lowerLeftID;//左下
+    uint32_t lowerRightID;//右下
 };
+
+static std::unique_ptr<can_plugins2::msg::Frame> valve_frame(const uint16_t id,uint8_t data[8])
+{
+  auto frame = std::make_unique<can_plugins2::msg::Frame>();
+  frame->id = id;
+  frame->is_rtr = false;
+  frame->is_extended = false;
+  frame->is_error = false;
+  frame->dlc = 8;
+  for(int i=0;i<8;i++){
+    frame->data[i] = data[i];
+  }
+  
+  return frame;
+}
