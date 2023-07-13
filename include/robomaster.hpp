fea8cf633bc03f70534f353f85758cea479f562d
yukichi6105 <107849799+YUKICHI6105@users.noreply.github.com>
2023-07-13 12:50:35 +0900

    robomaster_test_nodeの追加

---
+++ include/robomaster.hpp
@@ -0,0 +1,48 @@
+#include <rclcpp/rclcpp.hpp>
+#include <array>
+#include <can_plugins2/msg/frame.hpp>
+
+
+struct robomasterNumber{//ロボマスの番号
+  uint32_t upperRight;//右上
+  uint32_t upperLeft;//左上
+  uint32_t lowerLeft;//左下
+  uint32_t lowerRight;//右下
+};
+
+static std::unique_ptr<can_plugins2::msg::Frame> robomaster_frame(const uint16_t id,uint8_t data[8])
+{
+  auto frame = std::make_unique<can_plugins2::msg::Frame>();
+  frame->id = id;
+  frame->is_rtr = false;
+  frame->is_extended = false;
+  frame->is_error = false;
+  frame->dlc = 8;
+  for(int i=0;i<8;i++){
+    frame->data[i] = data[i];
+  }
+  
+  return frame;
+}
+
+void splitUint16(uint16_t input, uint8_t* outputArray,int number) {
+  // 上位ビットを取得して下位8ビットに格納
+  outputArray[number*2] = input >> 8;
+
+  // 下位ビットをビットマスクして上位8ビットに格納
+  outputArray[number*2-1] = input & 0xFF;
+}
+
+void formatvalue(float current,uint8_t value[8], int number){
+  if(current < 0){
+    float uncurrent = -current;
+    uint16_t uint16current = uncurrent/20*16384;
+    uint16current = uint16current | 0x8000 ;
+    std::cout << uint16current << std::endl;
+    splitUint16(uint16current,value,number);
+  }else{
+    uint16_t uint16current = current/20*16384;
+    std::cout << uint16current << std::endl;
+    splitUint16(uint16current,value,number);
+  }
+}
---
+++ src/robomaster_test.cpp
@@ -0,0 +1,84 @@
+#include <memory>
+#include <string>
+
+#include <chrono>
+#include <functional>
+
+#include <cstdint>
+
+#include <rclcpp/rclcpp.hpp>
+#include <can_plugins2/msg/frame.hpp>
+#include <sensor_msgs/msg/joy.hpp>
+#include "../include/can_utils.hpp"
+#include "../include/robomaster.hpp"
+//#include "../include/l4dc4_setting.hpp"
+
+using std::placeholders::_1;
+using namespace std::chrono_literals;
+
+class RoboMaster : public rclcpp::Node{
+    private:
+    void joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg);
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<can_plugins2::msg::Frame>::SharedPtr publisher_;
+    rclcpp::Subscription<sensor_msgs::msg::Joy>::SharedPtr subscriber_joy;
+    rclcpp::Subscription<can_plugins2::msg::Frame>::SharedPtr subscriber_can_rx;
+    size_t count_;
+    robomasterNumber robomaster;
+    float x=0.0; 
+    float y=0.0; 
+    float r=0.0;
+    
+    public:
+    RoboMaster() : Node("robomaster_test_node"), count_(0)
+    {
+    publisher_ = this->create_publisher<can_plugins2::msg::Frame>("can_tx", 10);
+    subscriber_joy = this->create_subscription<sensor_msgs::msg::Joy>("joy", 10, std::bind(&RoboMaster::joy_callback, this, _1));
+    subscriber_can_rx = this->create_subscription<can_plugins2::msg::Frame>("can_rx", 10, std::bind(&RoboMaster::can_callback, this, _1));
+    timer_ = this->create_wall_timer(200ms, std::bind(&RoboMaster::timer_callback, this));
+    this->declare_parameter("upperRight", 1);
+    this->declare_parameter("upperLeft", 2);
+    this->declare_parameter("lowerLeft", 3);
+    this->declare_parameter("lowerRight", 4);
+    }
+    void timer_callback();
+    void can_callback(const can_plugins2::msg::Frame msg);
+    void robomasterValuePublish(float upperRight,float upperLeft,float lowerLeft,float lowerRight);
+};
+
+void RoboMaster::timer_callback(){
+    robomaster.upperRight = this->get_parameter("upperRight").as_int();
+    robomaster.upperLeft = this->get_parameter("upperLeft").as_int();
+    robomaster.lowerLeft = this->get_parameter("lowerLeft").as_int();
+    robomaster.lowerRight = this->get_parameter("lowerRight").as_int();
+    robomasterValuePublish((y-x+r),(-x-y+r),(x-y+r),(x+y+r));
+}
+
+void RoboMaster::can_callback(const can_plugins2::msg::Frame msg){
+    RCLCPP_INFO(this->get_logger(),"test %u %u %u",msg.id,msg.data[0],msg.data[1]);
+}
+
+void RoboMaster::joy_callback(const sensor_msgs::msg::Joy::SharedPtr msg){
+    x= -(msg->axes[0]);
+    y=  (msg->axes[1]);
+    r= 0;
+}
+
+void RoboMaster::robomasterValuePublish(float currentUpperRight,float currentUpperLeft,float currentLowerLeft,float currentLowerRight){
+    uint8_t value[8];
+    formatvalue(currentUpperRight,value,robomaster.upperRight);
+    formatvalue(currentUpperLeft,value,robomaster.upperLeft);
+    formatvalue(currentLowerLeft,value,robomaster.lowerLeft);
+    formatvalue(currentLowerRight,value,robomaster.lowerRight);
+
+    //RCLCPP_INFO(this->get_logger(), "max speed %d!", test);
+    publisher_->publish(robomaster_frame(0x200, value));
+}
+
+int main(int argc, char * argv[])
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<RoboMaster>());
+    rclcpp::shutdown();
+    return 0;
+}
