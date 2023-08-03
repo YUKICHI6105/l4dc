392f65b653f6303261c3cb58ad1c66b84559379c
yukichi6105 <107849799+YUKICHI6105@users.noreply.github.com>
2023-08-04 01:36:10 +0900

    速度値に対する速度型PID制御の導入

--- src/robomaster_test.cpp
+++ src/robomaster_test.cpp
@@ -28,6 +28,7 @@ class RoboMaster : public rclcpp::Node{
     float x=0.0; 
     float y=0.0; 
     float r=0.0;
+    float roopTime = 0.001;
     
     public:
     RoboMaster() : Node("robomaster_test_node"), count_(0)
@@ -40,10 +41,15 @@ class RoboMaster : public rclcpp::Node{
     this->declare_parameter("upperLeft", 2);
     this->declare_parameter("lowerLeft", 3);
     this->declare_parameter("lowerRight", 4);
+    this->declare_parameter("urGainArray", rclcpp::PARAMETER_DOUBLE_ARRAY);
+    this->declare_parameter("ulGainArray", rclcpp::PARAMETER_DOUBLE_ARRAY);
+    this->declare_parameter("llGainArray", rclcpp::PARAMETER_DOUBLE_ARRAY);
+    this->declare_parameter("lrGainArray", rclcpp::PARAMETER_DOUBLE_ARRAY);
     }
     void timer_callback();
     void can_callback(const can_plugins2::msg::Frame msg);
     void robomasterValuePublish(float upperRight,float upperLeft,float lowerLeft,float lowerRight);
+    float calculatePID(float r,RobomasterFeedback feedback);
 };
 
 void RoboMaster::timer_callback(){
@@ -51,33 +57,49 @@ void RoboMaster::timer_callback(){
     feedback.upperLeft.number = this->get_parameter("upperLeft").as_int();
     feedback.lowerLeft.number = this->get_parameter("lowerLeft").as_int();
     feedback.lowerRight.number = this->get_parameter("lowerRight").as_int();
-    robomasterValuePublish((y-x+r),(-x-y+r),(x-y+r),(x+y+r));
+    feedback.upperRight.KP=this->get_parameter("urGainArray").as_double_array()[0];
+    feedback.upperRight.KI=this->get_parameter("urGainArray").as_double_array()[1];
+    feedback.upperRight.KD=this->get_parameter("urGainArray").as_double_array()[2];
+    feedback.upperLeft.KP=this->get_parameter("ulGainArray").as_double_array()[0];
+    feedback.upperLeft.KI=this->get_parameter("ulGainArray").as_double_array()[1];
+    feedback.upperLeft.KD=this->get_parameter("ulGainArray").as_double_array()[2];
+    feedback.lowerLeft.KP=this->get_parameter("llGainArray").as_double_array()[0];
+    feedback.lowerLeft.KI=this->get_parameter("llGainArray").as_double_array()[1];
+    feedback.lowerLeft.KD=this->get_parameter("llGainArray").as_double_array()[2];
+    feedback.lowerRight.KP=this->get_parameter("lrGainArray").as_double_array()[0];
+    feedback.lowerRight.KI=this->get_parameter("lrGainArray").as_double_array()[1];
+    feedback.lowerRight.KD=this->get_parameter("lrGainArray").as_double_array()[2];
+    robomasterValuePublish(feedback.upperRight.target,feedback.upperLeft.target,feedback.lowerLeft.target,feedback.lowerRight.target);
 }
 
 void RoboMaster::can_callback(const can_plugins2::msg::Frame msg){
     if(msg.id == (0x200+feedback.upperRight.number)){
         feedback.upperRight.locate = combineBytes(msg.data[0],msg.data[1]);
-        feedback.upperRight.speed = combineBytes(msg.data[2],msg.data[3]);
+        feedback.upperRight.speed = combineBytesToFloat(msg.data[2],msg.data[3])/60;
         feedback.upperRight.current = combineBytes(msg.data[4],msg.data[5]);
         feedback.upperRight.temperature = msg.data[6];
+        feedback.upperRight.target=feedback.upperRight.target+calculatePID((y-x+r),feedback.upperRight);
         RCLCPP_INFO(this->get_logger(),"ID %u current %u",msg.id,feedback.upperRight.current);
     }else if(msg.id == (0x200+feedback.upperLeft.number)){
         feedback.upperLeft.locate = combineBytes(msg.data[0],msg.data[1]);
-        feedback.upperLeft.speed = combineBytes(msg.data[2],msg.data[3]);
+        feedback.upperLeft.speed = combineBytesToFloat(msg.data[2],msg.data[3])/60;
         feedback.upperLeft.current = combineBytes(msg.data[4],msg.data[5]);
         feedback.upperLeft.temperature = msg.data[6];
+        feedback.upperLeft.target = feedback.upperLeft.target + calculatePID((-x-y+r),feedback.upperRight);
         RCLCPP_INFO(this->get_logger(),"ID %u current %u",msg.id,feedback.upperLeft.current);
     }else if(msg.id == (0x200+feedback.lowerLeft.number)){
         feedback.lowerLeft.locate = combineBytes(msg.data[0],msg.data[1]);
-        feedback.lowerLeft.speed = combineBytes(msg.data[2],msg.data[3]);
+        feedback.lowerLeft.speed = combineBytesToFloat(msg.data[2],msg.data[3])/60;
         feedback.lowerLeft.current = combineBytes(msg.data[4],msg.data[5]);
         feedback.lowerLeft.temperature = msg.data[6];
+        feedback.lowerLeft.target = feedback.lowerLeft.target + calculatePID((x-y+r),feedback.lowerLeft);
         RCLCPP_INFO(this->get_logger(),"ID %u current %u",msg.id,feedback.lowerLeft.current);
     }else if(msg.id == (0x200+feedback.lowerRight.number)){
         feedback.lowerRight.locate = combineBytes(msg.data[0],msg.data[1]);
-        feedback.lowerRight.speed = combineBytes(msg.data[2],msg.data[3]);
+        feedback.lowerRight.speed = combineBytesToFloat(msg.data[2],msg.data[3])/60;
         feedback.lowerRight.current = combineBytes(msg.data[4],msg.data[5]);
         feedback.lowerRight.temperature = msg.data[6];
+        feedback.lowerRight.target = feedback.lowerRight.target + calculatePID((x+y+r),feedback.lowerRight);
         RCLCPP_INFO(this->get_logger(),"ID %u current %u",msg.id,feedback.lowerRight.current);
     }
 }
@@ -107,6 +129,23 @@ void RoboMaster::robomasterValuePublish(float currentUpperRight,float currentUpp
     formatvalue(currentLowerRight,value,feedback.lowerRight.number);
     //RCLCPP_INFO(this->get_logger(), "max speed %u!", value[0]); 
     publisher_->publish(robomaster_frame(0x200, value));
+}
+
+float RoboMaster::calculatePID(float r,RobomasterFeedback feedback){
+    // 下記変数は既に与えられているものとする
+    // y       : 現在の出力
+    // r       : 現在の目標値
+    // e_pre   : 前回の誤差
+    // T       : 制御周期
+    // KP,KI,KD: P,I,Dゲイン
+
+  // PID制御の式より、制御入力uを計算
+  float e  = r - feedback.speed;                // 誤差を計算
+  float de = (e - feedback.e_pre)/roopTime;        // 誤差の微分を近似計算
+  feedback.ie = feedback.ie + (e + feedback.e_pre)*roopTime/2;       // 誤差の積分を近似計算
+  feedback.e_pre = e;
+  float u  = feedback.KP*e + feedback.KI*feedback.ie + feedback.KD*de; // PID制御の式にそれぞれを代入
+  return u;
 }
 
 int main(int argc, char * argv[])
